443d02cae0cf66b04623f0dbd635197db09de913
Luc Wallace <lucwallace06@gmail.com>
2024-08-07 15:05:32 +0100

    Update 014.py

--- 1-100/014.py
+++ 1-100/014.py
@@ -3,24 +3,24 @@ known_values: dict[int, int] = {}
 
 def collatz(n):
     if n == 1:
-        return 1, 1
+        return 1
 
     if n in known_values:
-        return 1, known_values[n]
+        return known_values[n]
 
     if n % 2 == 0:
-        num, terms = collatz(n / 2)
+        terms = collatz(n / 2)
     else:
-        num, terms = collatz(3 * n + 1)
+        terms = collatz(3 * n + 1)
     known_values[n] = terms + 1
-    return num, terms + 1
+    return terms + 1
 
 
 num = 0
 most_terms = 0
 
 for n in range(1, 1_000_001):
-    _, terms = collatz(n)
+    terms = collatz(n)
     if terms > most_terms:
         num = n
         most_terms = terms
